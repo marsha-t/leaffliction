d467139dbc497a084919b4608c16023ded7ba6a1
Raghda Yagoub <ryagoub@lab2r4s3.42abudhabi.ae>
2026-08-04 16:48:49 +0400

    distortion

---
+++ Augmentation/shear.py
@@ -0,0 +1,5 @@
+# def shear():
+#     """
+#     is the same as skewing 
+#     is the process of changing one axes base on the value of the other axes 
+#     """
--- Augmentation/skew.py
+++ Augmentation/skew.py
@@ -16,7 +16,7 @@ def skew(img):
         Return:
             skewed pillow image
     """
-    skewed_img = img.transform(img.size, Image.AFFINE, (1,0.5, 0,0,1,0))
+    skewed_img = img.transform(img.size, Image.AFFINE, (1,0, 0,0.5,1,0))
     fig, plots = plt.subplots(nrows=1, ncols=2, figsize=(15, 2))
     plots[0].imshow(img)
     plots[0].set_title("original")
